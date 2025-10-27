742ffadae9fb6f1fceb71a599ccab6c781d9de2e
cochearno <cochearno17@gmail.com>
2025-10-27 13:46:25 +0100

    add icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
-	compatibilityDate: "2025-07-15",
-	devtools: { enabled: true },
+ compatibilityDate: "2025-07-15",
+ devtools: { enabled: true },
+ modules: ["nuxt-svgo"],
 });
 ({
-	vite: {
-		css: {
-			preprocessorOptions: {
-				scss: {
-					additionalData: '@use "~/assets/main.scss" as *;',
+				vite: {
+								css: {
+												preprocessorOptions: {
+																scss: {
+																				additionalData: '@use "~/assets/main.scss" as *;',
+																},
+												},
+								},
 				},
-			},
-		},
-	},
+				svgo: {
+								autoImportPath: "./public/icons",
+								componentPrefix: "icon",
+				},
 });
-
