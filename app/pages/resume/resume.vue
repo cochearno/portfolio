332441803f28bf31a4bc8e6a1fb406df162d3881
cochearno <cochearno17@gmail.com>
2025-10-27 11:28:28 +0100

    add page contain

--- app/pages/resume/resume.vue
+++ app/pages/resume/resume.vue
@@ -1,6 +1,17 @@
 <template>
 	<div class="resume-page">
-		<div class="resume-page-title">Maxime Kuhn</div>
+		<div class="resume-page-title">
+			<h1>Maxime Kuhn</h1>
+			<span class="titre">Développeur Web Fullstack en formation </span>
+			<span class="accroche"
+				>Je concois des interfaces web modernes, accessibles et centrées
+				utilisateur</span
+			>
+		</div>
+		<div class="resume-page-contenu"><h2>Expériences Professionelles</h2></div>
+		<div class="resume-page-loisir">
+			<h2>Centre d'intérets</h2>
+		</div>
 	</div>
 </template>
 
