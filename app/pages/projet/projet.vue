2618aa284018b4495e19cd1ed6f249fbbc968079
cochearno <cochearno17@gmail.com>
2025-10-30 11:30:23 +0100

    add new links

--- app/pages/projet/projet.vue
+++ app/pages/projet/projet.vue
@@ -1,19 +1,25 @@
 <template>
 	<div class="project-page">
-		<div class="project-title-page">Mes projets réalisés</div>
+		<div class="project-title-page">Mes projets réalisés ou en cours</div>
 		<div class="project-line-two">
 			<div class="project-one">
-				<img
-					src="../../assets/img/capudculotte.png"
-					alt="logo-capudculotte"
-					class="capudculotte"
-				/>
+				<a href="https://github.com/cochearno/capudculotte" target="_blank">
+					<img
+						src="../../assets/img/capudculotte.png"
+						alt="logo-capudculotte"
+						class="capudculotte"
+					/>
+				</a>
 			</div>
-			<div class="project-two">two</div>
+			<div class="project-two">
+				<a href="" target="_blank"
+					><img
+						src="../../assets/img/baptservices.png"
+						alt="logo-baptservices"
+						class="baptservices"
+				/></a>
+			</div>
 			<div class="project-three">three</div>
-			<div class="project-four">four</div>
-			<div class="project-five">five</div>
-			<div class="project-six">six</div>
 		</div>
 	</div>
 </template>
